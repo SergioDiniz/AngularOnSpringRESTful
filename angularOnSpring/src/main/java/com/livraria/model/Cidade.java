1a96b7f0c000d040142d24b9feb2214420ec79d8
Sergio Diniz <sergiodinizsh@gmail.com>
2017-01-22 15:41:42 -0300

    modelo Cliente

---
+++ angularOnSpring/src/main/java/com/livraria/model/Cidade.java
@@ -0,0 +1,63 @@
+package com.livraria.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Cidade {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String nome;
+	
+	public Cidade() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Cidade(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ angularOnSpring/src/main/java/com/livraria/model/Cliente.java
@@ -0,0 +1,146 @@
+package com.livraria.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String nome;
+	private String cpf;
+	private String telefone;
+	private String sexo;
+	private Boolean ativo;
+	private String interesse;
+
+	@Column(columnDefinition = "text")
+	private String foto;
+
+	private String endereco;
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Estado estado;
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Cidade cidade;
+
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public String getInteresse() {
+		return interesse;
+	}
+
+	public void setInteresse(String interesse) {
+		this.interesse = interesse;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
