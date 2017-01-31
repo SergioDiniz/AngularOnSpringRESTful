bfd5ebb1e23d1f7b6aecd69056ca807cfe838d9e
Sergio Diniz <sergiodinizsh@gmail.com>
2017-01-31 10:50:36 -0300

    cadastrando fornecedor

--- angularOnSpring/src/main/webapp/modulos/admin/fornecedor-cadastro/fornecedor-cadastro-controller.js
+++ angularOnSpring/src/main/webapp/modulos/admin/fornecedor-cadastro/fornecedor-cadastro-controller.js
@@ -1,3 +1,27 @@
-angular.module("livraria.admin").controller("FornecedorCadastroController", function($scope) {
-	$scope.cadastro = "sergio"
+angular.module("livraria.admin").controller("FornecedorCadastroController", function($scope, $location, adminService, clienteService) {
+	$scope.fornecedor = {}
+	
+	
+	$scope.cadastrarFornecedor = function() {
+		adminService.cadastrarFornecedor($scope.fornecedor)
+		.then(function(response) {
+			alert("Novo Fornecedor Cadastrado com Sucesso!")
+			$location.path("/admin/fornecedor")
+		}).catch(function(response) {
+			alert("ERROR! " + response.data.error)
+		})
+	}
+	
+	$scope.carregarEstados = function() {
+		 clienteService.carregarEstados()
+		 .then(function(response) {
+			$scope.estados = response.data
+		 })
+	}
+	
+	$scope.carregarCidades = function(estado) {
+		clienteService.carregarCidades(estado.id).then(function(response) {
+			$scope.cidades = response.data
+		})
+	}
 })
