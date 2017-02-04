0304568f9f96fe04ed884eb4d0383ef0d4c03740
Sergio Diniz <sergiodinizsh@gmail.com>
2017-02-04 20:40:39 -0300

    somando valor total

--- angularOnSpring/src/main/webapp/modulos/cliente/carrinho/carrinho-cliente-controller.js
+++ angularOnSpring/src/main/webapp/modulos/cliente/carrinho/carrinho-cliente-controller.js
@@ -1,11 +1,27 @@
 angular.module("livraria.cliente").controller("CarrinhoClienteController", function($scope, clienteService) {
 	$scope.carrinho = clienteService.getCarrinho()
 	
+	
 	$scope.getValorTotal = function(valor, quantidade) {
 		return quantidade * parseFloat(valor)
 	}
 	
 	$scope.almentarQuantidade = function(indexItem, produto) {
 		clienteService.alterarQuantidadeEmProduto(indexItem, produto)
+		$scope.somarValorTotal()
+	}
+	
+	$scope.somarValorTotal = function() {
+		$scope.valorDaCompra = 0
+		
+		var carrinho = clienteService.getCarrinho()
+		
+		carrinho.forEach(function(produto, i, array) {
+			
+			$scope.valorDaCompra += produto.quantidade * parseFloat(produto.item.valor)
+			
+			console.log(produto.item.titulo)
+		})
+		
 	}
 })
